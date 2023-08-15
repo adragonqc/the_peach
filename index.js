0fa975a21219f3a4c407fff5ba348f8e89b61a69
Ariella Russin <ariella@russin.us>
2023-08-14 20:28:36 -0400

    praise works

--- index.js
+++ index.js
@@ -1,45 +1,27 @@
-// const Discord = require('discord.js');
-// const client = new Discord.Client({
-// 	intents: [
-// 		Discord.GatewayIntentBits.Guilds,
-// 		// Discord.GatewayIntentBits.GuildMessages,
-// 		// Discord.GatewayIntentBits.MessageContent,
-// 		// Discord.GatewayIntentBits.GuildMembers,
-// 	],
-// });
-
-// client.once('ready', () => {
-// 	console.log('Ready!');
-// });
-
-// client.login('');
-
-// client.on('message', message => {
-// 	console.log(message.content);
-// });
-const fs = require('node:fs');
-const path = require('node:path');
-const { Client, Collection, GatewayIntentBits } = require('discord.js');
+// Require the necessary discord.js classes
+const { Client, GatewayIntentBits } = require('discord.js');
 const { token } = require('./config.json');
 
-const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.GuildMembers, GatewayIntentBits.MessageContent] });
 
-client.commands = new Collection();
-const foldersPath = path.join(__dirname, 'commands');
-const commandFolders = fs.readdirSync(foldersPath);
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once('ready', (c) => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
+});
 
-for (const folder of commandFolders) {
-	const commandsPath = path.join(foldersPath, folder);
-	const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
-	for (const file of commandFiles) {
-		const filePath = path.join(commandsPath, file);
-		const command = require(filePath);
-		if ('data' in command && 'execute' in command) {
-			client.commands.set(command.data.name, command);
-		} else {
-			console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`);
-		}
+client.on('messageCreate', (message) => {
+	if (message.content === 'ping') {
+		message.channel.send(`🏓Latency is ${Date.now() - message.createdTimestamp}ms. API Latency is ${Math.round(client.ws.ping)}ms`);
 	}
-}
+});
 
+client.on('messageCreate', (message) => {
+	if (message.content === 'praise') {
+		message.channel.send('# PRAISE BE UPON THEM');
+	}
+});
+
+// Log in to Discord with your client's token
 client.login(token);
